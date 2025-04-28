48c9ae8c83926e6a931d90635fa838dfb700ff68
tatsiana_shulha <t.shulha@symfa.com>
2025-04-28 22:31:18 +0200

    feat: child processes

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,27 @@
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
+
+const filePath = 'files/script.js';
+const spawn = require('child_process').spawn;
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const child = spawn(`node`, [filePath, ...args], {
+        stdio: ['pipe', 'pipe', 'inherit'],
+    });
+
+    process.stdin.pipe(child.stdin);
+    child.stdout.pipe(process.stdout);
+
+    return new Promise((res, rej) => {
+        child.stdout.on('data', (data) => {
+            res(`data: ${data}`);
+        });
+        
+        child.on('exit', (code) => {
+            rej(`on exit ${code}`);
+        });
+    });
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(['fff', 'ggg', 'ttt']);
