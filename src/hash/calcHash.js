62ab0897e5e16ddb4258415564d18bbf3b6eec28
tatsiana_shulha <t.shulha@symfa.com>
2025-04-28 17:05:37 +0200

    feat: calculate hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,19 @@
+
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url);
+
+const fs = require('fs');
+const crypto = require('crypto');
+const stream = require('stream/promises');
+const filePath = 'files/fileToCalculateHashFor.txt';
+
 const calculateHash = async () => {
-    // Write your code here 
+    const fd = fs.createReadStream(filePath);
+    const hash = crypto.createHash('sha256');
+    await stream.pipeline(fd, hash);
+
+    console.log(hash.digest('hex'));
 };
 
 await calculateHash();
