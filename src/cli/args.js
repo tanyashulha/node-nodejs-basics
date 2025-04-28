86b567270c5c87e6442091e7f9c8b4741b98dfa4
tatsiana_shulha <t.shulha@symfa.com>
2025-04-28 07:12:08 +0200

    feat: command line interface

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,9 @@
 const parseArgs = () => {
-    // Write your code here 
+    const str = process.argv.slice(2).map((val, i) => {
+        return (i % 2) ? `is ${val},`  : val;
+    }).join(' ').toString();
+
+    console.log(str.slice(0, -1));
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,12 @@
 const parseEnv = () => {
-    // Write your code here 
+    const prefix = 'RSS_';
+    const keys = Object.entries(process.env).reduce((map, [key, value]) => {
+        if (key.indexOf(prefix) === 0) {
+            map[key] = value;
+        }
+        return map;
+    }, {})
+    console.log(keys);
 };
 
 parseEnv();
