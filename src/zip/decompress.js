4595c1dcb36cccff3949a09c5072376dc40ff04e
tatsiana_shulha <t.shulha@symfa.com>
2025-10-24 12:26:07 +0200

    implement decompess

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,18 @@
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url);
+const fs = require('fs');
+const zlib = require('zlib');
+
+const compressedFilePath = 'src/zip/files/archive.gz';
+const filePath = 'src/zip/files/fileToCompress.txt';
+
+const fileToDecompress = fs.createReadStream(compressedFilePath);
+const destination = fs.createWriteStream(filePath);
+
 const decompress = async () => {
-  // Write your code here
+  const decompress = zlib.createGunzip();
+  await fileToDecompress.pipe(decompress).pipe(destination);
 };
 
 await decompress();
