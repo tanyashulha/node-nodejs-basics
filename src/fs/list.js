522838a6322598ee098db8fe0e3a4bc81e103660
tatsiana_shulha <t.shulha@symfa.com>
2025-10-24 11:59:50 +0200

    add lits.js

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,18 @@
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url);
+const fs = require('fs/promises');
+
+const folderName = 'src/fs/files';
+
 const list = async () => {
-  // Write your code here
+  try {
+    await fs.access(folderName);
+    const files = await fs.readdir(folderName);
+    files.forEach(f => { console.log(f) });
+  } catch(e) {
+    throw new Error(errorMessage);
+  }
 };
 
 await list();
