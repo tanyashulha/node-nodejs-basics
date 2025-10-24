a190ea4b7104a08ec1489108235181bacbec63e3
tatsiana_shulha <t.shulha@symfa.com>
2025-10-24 12:25:53 +0200

    refactor

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -5,9 +5,10 @@ const fs = require('fs');
 const zlib = require('zlib');
 
 const filePath = 'src/zip/files/fileToCompress.txt';
+const compressedFilePath = 'src/zip/files/archive.gz';
 
 const fileToCompress = fs.createReadStream(filePath);
-const destination = fs.createWriteStream('src/zip/files/archive.gz');
+const destination = fs.createWriteStream(compressedFilePath);
 
 const compress = async () => {
   const compress = zlib.createGzip();
