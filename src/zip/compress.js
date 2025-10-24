70b1cb7cbd6aea0e3b94ba4433bbdbd1e347bba7
tatsiana_shulha <t.shulha@symfa.com>
2025-10-24 12:22:34 +0200

    implement compess

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,17 @@
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url);
+const fs = require('fs');
+const zlib = require('zlib');
+
+const filePath = 'src/zip/files/fileToCompress.txt';
+
+const fileToCompress = fs.createReadStream(filePath);
+const destination = fs.createWriteStream('src/zip/files/archive.gz');
+
 const compress = async () => {
-  // Write your code here
+  const compress = zlib.createGzip();
+  await fileToCompress.pipe(compress).pipe(destination);
 };
 
 await compress();
