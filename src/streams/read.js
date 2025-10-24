67194b19ca3de4f6140e5c0e26db467383c864f4
tatsiana_shulha <t.shulha@symfa.com>
2025-10-24 14:11:52 +0200

    add read strem

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,12 @@
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url);
+
+const fs = require('fs');
+const filePath = 'src/streams/files/fileToRead.txt';
+
 const read = async () => {
-  // Write your code here
+  fs.createReadStream(filePath, 'utf-8').pipe(process.stdout);
 };
 
 await read();
