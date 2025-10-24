4ebed6ed818c7d0e7cbc02e0a6116cc280d519d8
tatsiana_shulha <t.shulha@symfa.com>
2025-10-24 13:50:07 +0200

    implement write && transform streams

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,13 @@
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url);
+
+const fs = require('fs');
+const filePath = 'src/streams/files/fileToWrite.txt';
+
 const write = async () => {
-  // Write your code here
+  const file = fs.createWriteStream(filePath);
+  process.stdin.pipe(file);
 };
 
 await write();
